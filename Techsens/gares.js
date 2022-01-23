eaef0a671d3adbaef09af2b41b22693aa59b6297
YNSBNL <92267359+YNSBNL@users.noreply.github.com>
2022-01-23 16:07:22 +0100

    gares

--- Techsens/gares.js
+++ Techsens/gares.js
@@ -1,13 +1,12 @@
-
-var arr_num = Array.from({length: 4}, () => getRandomInt();
-    const firstNumber = arr_num[arr_num.length - 1];
+var arr_num = Array.from({length: 4}, () => (Math.random() * (900 - 600 + 1) + 600));
+    const firstNumber = arr_num[arr_num.length - 1]
     const secondNumber = arr_num[arr_num.length - 2];
     const thirdNumber = arr_num[arr_num.length - 3];
     const fourthNumber = arr_num[arr_num.length - 4];
     
     
 
-var currentdate = new Date(); 
+    var currentdate = new Date(); 
 var datetime = currentdate.getDate() + "/"
                 + (currentdate.getMonth()+1)  + "/" 
                 + currentdate.getFullYear() + " @ "  
@@ -29,7 +28,7 @@ function run(){
     var counter =0;
     var grid = document.getElementById("grid");
     for (var i = 0, row; row = grid.rows[i]; i++){
-        row.cells[0].textContent = arr_num[getRandomInt()];
+        row.cells[0].textContent = arr_num[getRandom()];
         row.cells[1].textContent = datetime;
         row.cells[2].textContent = average;
         row.cells[3].textContent = Math.sqrt(average);
@@ -39,40 +38,31 @@ function run(){
     }   
 }
 
-/*
-function getRandomInt(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min +1)) + min;
-
-}*/
-
 function getRandom(){
     return Math.floor(Math.random() * arr_num.length) + 0 ;
     
 }
+var xValues = ["1ère mesure", "2ème mesure", "3ème mesure", "4ème mesure"];
+        var yValues = [firstNumber, secondNumber, thirdNumber, fourthNumber];
+        var barColors = ["red", "green","blue","orange","brown"];
 
-		var xValues = ["1ère mesure", "2ème mesure", "3ème mesure", "4ème mesure"];
-		var yValues = [firstNumber, secondNumber, thirdNumber, fourthNumber];
-		var barColors = ["red", "green","blue","orange","brown"];
-
-		new Chart("myChart", {
-		  type: "bar",
-		  data: {
-		    labels: xValues,
-		    datasets: [{
-		      backgroundColor: barColors,
-		      data: yValues
-		    }]
-		  },
-		  options: {
-		    legend: {display: false},
-		    title: {
-		      display: true,
-		      text: "Graphique des données sur les particules fines (en quelques secondes)"
-		    }
-		  }
-		});
+        new Chart("myChart", {
+          type: "bar",
+          data: {
+            labels: xValues,
+            datasets: [{
+              backgroundColor: barColors,
+              data: yValues
+            }]
+          },
+          options: {
+            legend: {display: false},
+            title: {
+              display: true,
+              text: "Graphique des données sur les particules fines (en quelques secondes)"
+            }
+          }
+        });
 
   var on = 0; // 1 is true, 0 is false
 
@@ -93,11 +83,3 @@ function getRandom(){
          on = 0; //You forgot a ; here
        }
       }
- 
-   
-        
-       
-
-
-             
-    
