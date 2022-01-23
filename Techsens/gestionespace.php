c973f839ec12b6d48019134366f78feb12ec8a03
khalilsammoud <sammoudkhalil@gmail.com>
2022-01-24 00:59:14 +0100

    m

---
+++ Techsens/gestionespace.php
@@ -0,0 +1,114 @@
+<?php
+include "config2.php";
+
+
+
+
+
+?>
+<!doctype html>
+<html>
+
+<head>
+    <meta charset="utf-8" />
+    <link href="graphique.css" media="all" rel="stylesheet" type="text/css">
+</head>
+
+<body>
+
+       <?php
+         if(!isset($_SESSION)){
+            session_start();
+         }
+         if (isset($_SESSION['connected']) && $_SESSION['connected'] == true){
+            include("./Composants/headerconnected.php");
+                }
+        else {include("./Composants/headerdeconnected.php");
+    }
+    ?>
+
+    <div class="container2">
+        <center>
+            <p class="contacter2"> Espace GESTIONNAIRE </p>
+        </center>
+        <span>
+
+            <div class="formulaire2">
+                <form class="intro">
+                    <p class="txt1"> Vos Informations Personnelles </p>
+
+                    <?php
+
+                    if (isset($_GET['Logout'])) {
+                        if ($_GET['Logout'] == true) {
+                            session_unset();
+                            header("location: adminid.php");
+                        }
+                    } else if ($_SESSION['uname'] !== "") {
+                        $user = $_SESSION['uname'];
+
+
+
+                        // afficher un message
+                        echo "<br>Bonjour $user, vous êtes connecté";
+                    }
+                    ?>
+                    <br><br>
+                    <a href="aide.php" class="button2">Information incorrecte ? Contactez-nous !</a>
+                    <div class="texteleft">
+        
+                </form>
+
+            </div>
+    </div>
+        <p class="pdp">Photo de profil : </p>
+    <img src="anonyme.png" alt="photo de profil" class="img30" />
+
+
+    <div><center><h2>Moteur de recherche : trouvez les informations de l'adhérent souhaité en écrivant son login (username)</h2></center></div>
+    <form  action='config3.php' method='POST'>
+        <label for="user">Username:</label> <br/>
+        <input type='text' name='user' id="user"  ><br/>
+        
+        <input type='submit' name='search' id="search" />
+    </form>
+
+<div class="footer">
+        <div class="foot">Techsens est une société cherchant à développer des solutions en accord avec le développement durable.</div>
+        <img src="logoo.png" alt="photo du logo" />
+        
+        <div class="content">
+            <div class="barre">
+            <p>---------------------------------------------------</p></div>
+            <div class="photologo">
+            <div class="logofb">
+                <a href="https://www.facebook.com/profile.php?id=100073322512659"> <img src="fb.png" alt="logo fb" /> </p></a></div>
+            <div class="logoinsta">
+                <a href="https://www.instagram.com/"> <img src="insta.png" alt="logo insta" /> </a></div>
+            <div class="logotwitter">
+                <a href="https://twitter.com/Techsens_"> <img src="twitter.png" alt="logo twitter" /></a></div>
+            <div class="logolinkedin">
+                <a href="https://www.linkedin.com/in/techsens-a9887b222"> <img src="linkedin.png" alt="logo linkedin" /> </a></div>
+        </div></div>
+            <div class="end">
+                    <label for="end"> 
+                        <a href="cgu.html"> Mentions légales et politique de cofidentialité</a></label></div>
+    </div>
+    <div id="scrollUp">
+            <a href="#haut" class="top">
+                <img src="Haut.png" alt="photo bouton" class="img10" /></a>
+        </div>
+
+
+</body>
+
+
+
+
+
+
+
+
+
+
+</html>
