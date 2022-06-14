6d13575e868711e2f8b8335f96da15063d51862c
Audreeey <92269720+Audreeey@users.noreply.github.com>
2022-06-14 09:24:51 +0200

    Update corentin_celton.php

--- Techsens/corentin_celton.php
+++ Techsens/corentin_celton.php
@@ -127,19 +127,19 @@ $ch = curl_init();
 curl_setopt(
     $ch, 
     CURLOPT_URL,
-    "http://projets-tomcat.isep.fr:8080/appService?ACTION=GETLOG&TEAM=9999"
+    "http://projets-tomcat.isep.fr:8080/appService/?ACTION=GETLOG&TEAM=0G6B"
 );
 curl_setopt($ch, CURLOPT_HEADER, FALSE);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
 $data = curl_exec($ch);
 curl_close($ch);
-echo "Raw Data:<br />";
-echo("$data");
+echo "Raw Data:<br /><br/>";
+echo("$data<br/>");
 
 
 // METTRE LES DONNES SOUS FORME DE TABLEAU
 $data_tab = str_split($data,33);
-echo "Tabular Data:<br />";
+echo "<br/> Tabular Data:<br /><br/>";
 for($i=0, $size=count($data_tab); $i<$size; $i++){
 echo "Trame $i: $data_tab[$i]<br />";
 }
@@ -147,7 +147,7 @@ echo "Trame $i: $data_tab[$i]<br />";
 
 
 //DECODER 1 TRAME
-/*
+
 
 $trame = $data_tab[1];
 // décodage avec des substring
@@ -158,7 +158,7 @@ $o = substr($trame,1,4);
 list($t, $o, $r, $c, $n, $v, $a, $x, $year, $month, $day, $hour, $min, $sec) = 
 sscanf($trame,"%1s%4s%1s%1s%2s%4s%4s%2s%4s%2s%2s%2s%2s%2s");
 echo("<br />$t,$o,$r,$c,$n,$v,$a,$x,$year,$month,$day,$hour,$min,$sec<br />");
-*/
+
 ?>
 <br>
 <br>
